babfe2f424f6ae9e734c55bd89bd260fbcf067e5
MikePai <ye518612@gmail.com>
2020-10-03 11:47:12 +0800

    Initial Commit

---
+++ ConanPicture/ConanPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConanPictureApp.swift
+//  ConanPicture
+//
+//  Created by 白謹瑜 on 2020/10/3.
+//
+
+import SwiftUI
+
+@main
+struct ConanPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConanPicture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConanPicture
+//
+//  Created by 白謹瑜 on 2020/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
